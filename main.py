3113d6f714a4111e0fb1ea147edf866915c295b6
Robert Sigler <sigler@improvisedscience.org>
2020-04-25 20:45:33 -0400

    Fixing up formatting

--- main.py
+++ main.py
@@ -23,15 +23,17 @@ class GuessTheWeight(Gtk.Window):
         self.add(mainBox)
 
         # Create a vertical box (col 01 of mainBox)
-        selectorBoxes = Gtk.Box(spacing=6, orientation=Gtk.Orientation.VERTICAL)
+        selectorBoxes = Gtk.Box(
+            spacing=6, orientation=Gtk.Orientation.VERTICAL)
         mainBox.pack_start(selectorBoxes, False, False, 0)
-        
+
         # Create a frame for the bar weight
         chooseBarFrame = Gtk.Frame(label="Select a bar weight")
         selectorBoxes.pack_start(chooseBarFrame, False, False, 0)
 
         # Create a horizontal box for the radio buttons
-        barWeightBox = Gtk.Box(spacing=6, orientation=Gtk.Orientation.HORIZONTAL)
+        barWeightBox = Gtk.Box(
+            spacing=6, orientation=Gtk.Orientation.HORIZONTAL)
         chooseBarFrame.add(barWeightBox)
 
         # Create the bar weight radio buttons
@@ -39,22 +41,25 @@ class GuessTheWeight(Gtk.Window):
         for bar in weights.weights["bars"]:
             # If the buttonGroup has not been set (first button), create it
             if not buttonGroup:
-                buttonGroup = Gtk.RadioButton.new_with_label_from_widget(None, f"{str(bar)} lbs bar")
-                buttonGroup.connect("toggled", self.on_bar_weight_selected, bar)
+                buttonGroup = Gtk.RadioButton.new_with_label_from_widget(
+                    None, f"{str(bar)} lbs bar")
+                buttonGroup.connect(
+                    "toggled", self.on_bar_weight_selected, bar)
                 barWeightBox.pack_start(buttonGroup, False, False, 0)
-                
+
                 # Default to the first weight
                 self.barWeight = bar
             else:
                 # Create subsequent button to the buttonGroup
-                btn = Gtk.RadioButton.new_with_label_from_widget(buttonGroup, f"{str(bar)} lbs bar")
+                btn = Gtk.RadioButton.new_with_label_from_widget(
+                    buttonGroup, f"{str(bar)} lbs bar")
                 btn.connect("toggled", self.on_bar_weight_selected, bar)
                 barWeightBox.pack_start(btn, False, False, 0)
-        
+
         # Create a frame for the target weight
         chooseWeightFrame = Gtk.Frame(label="Select a target weight")
         selectorBoxes.pack_start(chooseWeightFrame, False, False, 0)
-        
+
         # Get the max weight possible (heaviest bar + all plates)
         maxWeight = int(weights.maxWeight())
         # Set the target weight as half the max weight
@@ -67,10 +72,11 @@ class GuessTheWeight(Gtk.Window):
             upper=maxWeight,
             step_increment=5,
             page_increment=10)
-        
+
         # Create a scale/slider
         weight_scale = Gtk.Scale(
-            orientation=Gtk.Orientation.HORIZONTAL, adjustment=targetWeightAdjustment)
+            orientation=Gtk.Orientation.HORIZONTAL,
+            adjustment=targetWeightAdjustment)
         weight_scale.connect("value-changed", self.on_weight_scale_moved)
         # set digits to 0 (integer)
         weight_scale.set_digits(0)
@@ -90,8 +96,6 @@ class GuessTheWeight(Gtk.Window):
         self.maxTriesSpinbutton.set_adjustment(maxTriesAdjustment)
         self.maxTriesSpinbutton.set_numeric(True)
         maxTriesFrame.add(self.maxTriesSpinbutton)
-
-        
 
         # Create the "calculate" button
         calculateButton = Gtk.Button.new_with_label("Calculate")
@@ -115,7 +119,7 @@ class GuessTheWeight(Gtk.Window):
         # Create a text cell renderer
         renderer_text = Gtk.CellRendererText()
 
-        # Create the "Weight" (plate name) column 
+        # Create the "Weight" (plate name) column
         key_text = Gtk.TreeViewColumn("Weight", renderer_text, text=0)
         treeview.append_column(key_text)
 
@@ -132,18 +136,18 @@ class GuessTheWeight(Gtk.Window):
     def on_weight_scale_moved(self, widget):
         # Set the targetWeight class prop to the slider value
         self.targetWeight = float(widget.get_value())
-    
+
     # When a bar weight radio button is selected
     def on_bar_weight_selected(self, widget, data):
         # Set the barWeight class prop to the slider value
         self.barWeight = data
-    
+
     # When the "calculate" button is clicked
     def on_click_calculate(self, widget):
 
         # Empty the solutionStore (this clearing the view)
         self.solutionStore.clear()
-        
+
         # Set the "status" text
         self.solutionLabel.set_text("Calculating...")
 
@@ -153,7 +157,7 @@ class GuessTheWeight(Gtk.Window):
             self.targetWeight,
             int(self.maxTriesSpinbutton.get_value())
         )
-        
+
         if solution:
             # If a solution is found...
 
@@ -161,21 +165,23 @@ class GuessTheWeight(Gtk.Window):
             tries = "tries"
             if solution['tries'] == 1:
                 tries = "try"
-            self.solutionLabel.set_text(f"Finished in {solution['tries']} {tries}")
+            self.solutionLabel.set_text(
+                f"Finished in {solution['tries']} {tries}")
 
             # Var for the total weight
             # Start with the bar weight
             totalWeight = self.barWeight
-            
+
             for plate in solution["solution"]:
                 # For each plate in the solution...
 
                 # Add the weight plate and count to the solutionStore
-                self.solutionStore.append([str(plate["weight"]), plate["count"]])
-                
+                self.solutionStore.append(
+                    [str(plate["weight"]), plate["count"]])
+
                 # Update the total weight
                 totalWeight = totalWeight + (plate["weight"] * plate["count"])
-            
+
             # Add the total weight to the solutionStore
             self.solutionStore.append(["Total", float(totalWeight)])
         else:
@@ -183,7 +189,8 @@ class GuessTheWeight(Gtk.Window):
 
             # Set the "status" text
             self.solutionLabel.set_text("Unable to find a solution :(")
-            
+
+
 # Start the stuff!
 win = GuessTheWeight()
 win.connect("destroy", Gtk.main_quit)
